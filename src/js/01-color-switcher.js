ba27da0df9415a1437459485ed624311640d3a7d
Volodymyr <110925314+woland31252@users.noreply.github.com>
2023-08-01 18:59:20 +0300

    add class

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,17 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerID = null;
 
+startBtn.addEventListener("click", handleStart);
+function handleStart() {
+    timerID = setInterval(getRandomHexColor(), 1000);
+    console.log("START!!!")
+}
+stopBtn.addEventListener("click", handleStop);
+function handleStop() {
+    clearInterval(timerID);
+    console.log("STOP!!")
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
