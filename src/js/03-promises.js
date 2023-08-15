fa310d9016181adb647c81d3b2bdf416a8bf4560
Volodymyr <110925314+woland31252@users.noreply.github.com>
2023-08-15 19:09:00 +0300

    function - promise

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,29 @@
+
+const form = document.querySelector(".form");
+const button = document.getElementById("btn");
+
+form.addEventListener("submit", onPromise);
+
+function onPromise(event) {
+  event.preventDefault();
+  const { delay, step, amount } = event.currentTarget;
+  let delayValue = Number(delay.value);
+  let stepDelay = Number(step.value);
+  let amountValue = Number(amount.value);
+  for (let position = 1; position <= amountValue; position++);
+  createPromise(position, delayValue);
+  delayValue += stepDelay
+}
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+};
+
